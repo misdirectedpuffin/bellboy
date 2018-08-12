a0b1539ddcdcfbceb602b9e1c070828a6fe521a3
Steven Carey <steven.carey@skyscanner.net>
2018-08-12 01:19:27 +0100

    Add asyncio to cli

--- trivago/cli.py
+++ trivago/cli.py
@@ -1,21 +1,20 @@
 """Console script for trivago."""
+import asyncio
 import os
 import sys
+from functools import update_wrapper
 
+import aiohttp
 import click
 
 from trivago.export import JsonExport, XmlExport, factory
 from trivago.transform import Row, load
-
-
-def get_current_directory():
-    """Make path to directory containing current file."""
-    return os.path.dirname(os.path.realpath(__file__))
+from trivago.validate import HttpUriValidator
 
 
 def output_directory(ctx, param, value):
     """Make path to data directory."""
-    return os.path.join(get_current_directory(), '..', 'data')
+    return os.path.join(os.getcwd(), 'data')
 
 
 def make_file_name(outfile, output_format):
@@ -23,12 +22,33 @@ def make_file_name(outfile, output_format):
     return '.'.join([outfile, output_format])
 
 
+def write(rows, should_write, outfile, data_dir, output_format):
+    if all([should_write, outfile]):
+        filename = make_file_name(outfile, output_format)
+        outpath = os.path.join(data_dir, filename)
+        ExportHandler = factory(output_format)
+        export = ExportHandler(rows)
+        export.write(outpath)
+
+
+def coro(f):
+    f = asyncio.coroutine(f)
+
+    def wrapper(*args, **kwargs):
+        loop = asyncio.get_event_loop()
+        return loop.run_until_complete(f(*args, **kwargs))
+    return update_wrapper(wrapper, f)
+
+
 @click.group()
-def entrypoint():
+@click.pass_context
+def entrypoint(ctx):
     """Entrypoint to CLI."""
+    ctx.obj = asyncio.get_event_loop()
 
 
 @entrypoint.command()
+@click.option('--ping', is_flag=True, default=False)
 @click.option('--write', is_flag=True, default=False)
 @click.option('--outfile', default='hotels')
 @click.option('--infile', default='hotels.csv')
@@ -42,21 +62,24 @@ def entrypoint():
     type=click.Choice(['json', 'xml']),
     default='json'
 )
-def parse(output_format, data_dir, infile, outfile, write):
+@coro
+@click.pass_obj
+async def parse(loop, output_format, data_dir, infile, outfile, write, ping):
     """Parse the csv"""
     data = load(os.path.join(data_dir, infile))
     rows = (row for row in data if row.is_valid())
 
-    if all([write, outfile]):
-        filename = make_file_name(outfile, output_format)
-        outpath = os.path.join(data_dir, filename)
-        click.echo(outpath)
-        click.echo(filename)
-        ExportHandler = factory(output_format)
-        export = ExportHandler(rows)
-        return export.write(outpath)
-    click.echo('You didn\'t specify whether or where to write to a file....')
+    if ping:
+        confirmation = click.confirm(
+            'You are about to make a large number of http requests. '
+            'Are you sure you want to continue?',
+            default='Yes'
+        )
 
+        if confirmation:
+            connector = aiohttp.TCPConnector(verify_ssl=False)
+            validator = HttpUriValidator(rows, loop, connector)
+            return await validator.fetch_all()
 
 if __name__ == "__main__":
-    sys.exit(entrypoint())  # pragma: no cover
+    sys.exit(entrypoint({}))  # pragma: no cover
